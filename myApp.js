4867887c95cafe166313501b075c661adae6bfbb
Dhruv Khara <66153057+BeLazy167@users.noreply.github.com>
2022-06-20 15:52:11 +0000

    completed the free code camp express

--- myApp.js
+++ myApp.js
@@ -1,25 +1,70 @@
 let express = require('express');
 let app = express();
+require('dotenv').config();
+let bodyParser = require('body-parser');
+let tosend = {"message": "Hello json"};
+let tosendupper = {"message": "HELLO JSON"};
+
+app.use((request,res,next)=>{
+	console.log(request.method + ' ' + request.path+' - ' + request.ip);
+	next();
+});
+
+
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.use("/public",express.static(__dirname + "/public"));
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+
+app.get("/json",(req,res)=>{
+	
+	if(process.env.MESSAGE_STYLE ==='uppercase'){
+		res.json(tosendupper);
+	}
+	else{
+		res.json(tosend);
+	}
+	
+});
+
+app.get('/now',(req,res,next)=>{
+	req.time = new Date().toString();
+	next()
+},(req,res)=>{
+	res.json({time: req.time});
+});
+
+
+
+
+app.get('/:word/echo' , (req,res)=>{
+	word = req.params.word
+	res.json({echo: word })
+})
 
 
 
 
 
+app.get('/name',(req,res)=>{
+	first = req.query.first;
+	last = req.query.last;
+	res.json({name : first +' '+ last})
+});
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
+app.post('/name', bodyParser.urlencoded({extended:false}) , (req,res)=>{
+	first = req.body.first;
+	last = req.body.last;
+	res.json({name : first +' '+ last})
+	
+})
 
 
 
